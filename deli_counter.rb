c785b482ba2616fe55163e826c8dec855f188919
patrickp246@gmail.com <patrickp246@gmail.com>
2018-09-09 15:44:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,9 +12,12 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1}"
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1 in line.}"
 end
 
-def now_serving
-  
+def now_serving(katz_deli)
+  if katz_deli.count < 1
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}."
 end
