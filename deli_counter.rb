fbf608ead849840829ddd184916a20f2d8860c06
patrickp246@gmail.com <patrickp246@gmail.com>
2018-09-09 15:41:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number "
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1}"
 end
 
 def now_serving
