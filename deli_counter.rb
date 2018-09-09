2ec17eb72c938a7e42309924b9510ca01c52361b
patrickp246@gmail.com <patrickp246@gmail.com>
2018-09-09 15:37:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,12 @@
 def line(katz_deli)
   if katz_deli.count < 1
-    "The line is currently empty."
-  end
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    katz_deli.each_with_index do |person,index|
+      current_line += " #{index + 1}. #{person}"
+    end
+    puts current_line
 end
 
 def take_a_number(katz_deli, name)
